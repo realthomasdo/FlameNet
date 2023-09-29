3d2c7e7a5ac661f63d5a7a432e4f2cbd1628b573
joshuamd21 <joshua.downey21@gmail.com>
2023-09-29 14:34:42 -0500

    setup basic beacon and signal prefabs
    the beacon now is able to spawn signals which hold packets that have information about their device origin and the sensor information held.
    
    currently beacons cant actually talk to each other yet.
    they can just send signals not receive them.

--- SIM/FlameNetSimulation/Assets/Scripts/Controllers/BeaconController.cs
+++ SIM/FlameNetSimulation/Assets/Scripts/Controllers/BeaconController.cs
@@ -2,21 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct Date
+{
+    public int day;
+    private int month;
+    private int time; // 24 hour time 1200 is noon etc
+}
 public struct SensorInformation
 {
-    private float temp;
-    private float humidity;
-    private Vector2 windDirection;
-    private float windSpeed;
+    public Date time;
+    public float temp;
+    public float humidity;
+    public Vector2 windDirection;
+    public float windSpeed;
 }
 public class BeaconController : MonoBehaviour
 {
+    private int beaconID;
     private bool isOn;
     private List<SensorInformation> data;
     // Start is called before the first frame update
     void Start()
     {
+        beaconID = 1;
+        data = new List<SensorInformation>();
+        data.Add(new SensorInformation { });
         setPower(true);
+        StartCoroutine(SpawnSignal());
     }
     public void setPower(bool isOn)
     {
@@ -33,5 +45,18 @@ public class BeaconController : MonoBehaviour
     }
     private void CollectData()
     {
+    }
+    private IEnumerator SpawnSignal()
+    {
+        while (true)
+        {
+            Packet packet = new Packet
+            {
+                beaconID = beaconID,
+                info = data[0],
+            };
+            SignalFactory.CreateSignal(transform.position, packet, 5);
+            yield return new WaitForSeconds(2);
+        }
     }
 }
---
+++ SIM/FlameNetSimulation/Assets/Scripts/Controllers/SignalController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Packet
+{
+    public int beaconID;
+    public SensorInformation info;
+}
+public class SignalController : MonoBehaviour
+{
+    [SerializeField] private float maxDistance;
+    private float distanceTraveled;
+    [SerializeField] private float growthSpeed;
+    [SerializeField] private Renderer myModel;
+    private Packet packet;
+
+    public void SetValues(Packet packet, float maxDistance)
+    {
+        this.packet = packet;
+        this.maxDistance = maxDistance;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale += transform.localScale * growthSpeed * Time.deltaTime;
+        distanceTraveled += transform.localScale.x * growthSpeed * Time.deltaTime / 2;
+        Color matColor = myModel.material.color;
+        matColor.a = 1 - distanceTraveled / maxDistance;
+        myModel.material.color = matColor;
+        if (maxDistance <= distanceTraveled)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
