2772047d8a8945cbe8dab45571f4b79eecc9c015
Himel-M <h1m37.m@gmail.com>
2023-10-30 12:52:35 -0500

    Postman mock api integration with table

--- GUI/flamenet-client/src/components/Table.js
+++ GUI/flamenet-client/src/components/Table.js
@@ -1,70 +1,11 @@
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { MaterialReactTable } from 'material-react-table';
 
 //nested data is ok, see accessorKeys in ColumnDef below
 //example data 
 //airqual in PM2.5 or PM10, micrograms/m^3
-const data = [
-  {
-    id: {
-      dataID: '1',
-      nodeID: '1',
-    },
-    timestamp: '6:00',
-    temperature: '80',
-    humidity: '40',
-    latitude: '30.591733619',
-    longitude: '-96.330444763',
-    airqual: '99',
-  },
-  {
-    id: {
-      dataID: '2',
-      nodeID: '2',
-    },
-    timestamp: '6:10',
-    temperature: '77',
-    humidity: '38',
-    latitude: '30.596417',
-    longitude: '-96.335634',
-    airqual: '80',
-  },{
-    id: {
-      dataID: '2',
-      nodeID: '2',
-    },
-    timestamp: '6:20',
-    temperature: '69',
-    humidity: '60',
-    latitude: '30.356655',
-    longitude: '-96.45454',
-    airqual: '110',
-  },{
-    id: {
-      dataID: '3',
-      nodeID: '3',
-    },
-    timestamp: '6:30',
-    temperature: '90',
-    humidity: '55',
-    latitude: '30.5454524',
-    longitude: '-96.42333',
-    airqual: '120',
-  },{
-    id: {
-      dataID: '4',
-      nodeID: '4',
-    },
-    timestamp: '6:40',
-    temperature: '104',
-    humidity: '50',
-    latitude: '30.59456456',
-    longitude: '-96.453345',
-    airqual: '150',
-  },
-];
-
 const DataTable = () => {
+  const[data, setData] = useState([]);
   //should be memoized or stable
   const columns = useMemo(
     () => [
@@ -112,6 +53,18 @@ const DataTable = () => {
     ],
     [],
   );
+
+  useEffect(() => {
+    fetch('https://87b96220-8f88-4790-833e-fc196aa3e959.mock.pstmn.io/fakeapidatabase/table')
+    .then((response) => response.json())
+    .then((apiData) =>{
+      console.log(apiData);
+      setData(apiData);
+    })
+    .catch((error) => {
+      console.error('Data request error: ', error);
+    });
+  }, []);
 
   return <MaterialReactTable columns={columns} data={data} />;
 };
